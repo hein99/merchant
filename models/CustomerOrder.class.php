35ee694b7a29fd69013f588823e1efcb021f6a0a
HeinKhant <heinkaungkhant1999@gmail.com>
2020-05-26 20:15:14 +0630

    Change Specification and Code in models file

---
+++ models/CustomerOrder.class.php
@@ -0,0 +1,21 @@
+<?php
+class CustomerOrder extends DataObject
+{
+  protected $data = array(
+    'id' => '';
+    'customer_id' => '';
+    'product_link' => '';
+    'remark' => '';
+    'quantity' => '';
+    'price' => '';
+    'us_tax' => '';
+    'mm_tax' => '';
+    'commission' => '';
+    'weight_cost' => '';
+    'order_status' => '';
+    'product_shipping_status' => '';
+    'created_date' => ''
+  );
+
+}
+ ?>
---
+++ models/Membership.class.php
@@ -0,0 +1,14 @@
+<?php
+class Membership extends DataObject
+{
+  protected $data = array(
+    'id' => '',
+    'name' => '',
+    'definition' => '',
+    'percentage' => '',
+    'modified_date' => ''
+  );
+
+  
+}
+ ?>
