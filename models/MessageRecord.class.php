9cd1b9b84e06b28d0ee66373390f63e56b82377e
SweSwe-Nyein <sweswe4720@gmail.com>
2020-05-27 21:22:30 +0630

    update model

--- models/MessageRecord.class.php
+++ models/MessageRecord.class.php
@@ -39,11 +39,32 @@
     {
       $conn = parent::connect();
       $sql = 'INSERT INTO '.TBL_MESSAGE_RECORD.' (to_user_id, from_user_id, messages, image, arrived_time, status)
-              VALUES (:to_user_id, :from_user_id, :messages, image, NOW(), 0)';
+              VALUES (:to_user_id, :from_user_id, :messages, :image, NOW(), 0)';
+      try {
+        $st = $conn->prepare($sql);
+        $st->bindValue(':to_user_id', $this->data['to_user_id'], PDO::PARAM_INT);
+        $st->bindValue(':from_user_id', $this->data['from_user_id'], PDO::PARAM_INT);
+        $st->bindValue(':messages', $this->data['messages'], PDO::PARAM_STR);
+        $st->bindValue(':image', $this->data['image'], PDO::PARAM_STR);
+        $st->execute();
+        parent::disconnect($conn);
+      }catch (PDOException $e) {
+        parent::disconnect($conn);
+        die('Query failed: ' . $e->getMessage());
+      }
     }
     public static function countUnseenMessage($from_user_id, $to_user_id)
     {
-
+      $conn = parent::connect();
+      $sql = 'SELECT * FROM '.TBL_MESSAGE_RECORD.' WHERE from_user_id = :from_user_id AND to_user_id = :to_user_id AND status = 0';
+      try {
+        $st = $conn->prepare($sql);
+        $st->bindValue(':from_user_id', $from_user_id, PDO::PARAM_INT);
+        $st->bindValue(':to_user_id', $to_user_id, PDO::PARAM_INT);
+        $row = $st->fetchAll();
+        $count = $row->rowCount();
+        if($count > 0) return $count;
+      }
     }
   }
 
