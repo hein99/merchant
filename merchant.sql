76dadb9beaf78a4d055e1a0b173de9bc4a6d8ea2
HeinKhant <heinkaungkhant1999@gmail.com>
2020-05-28 21:25:55 +0630

    db update and order.php file update

--- merchant.sql
+++ merchant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 26, 2020 at 07:21 PM
+-- Generation Time: May 28, 2020 at 04:54 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -41,8 +41,21 @@ CREATE TABLE `customer_order` (
   `weight_cost` float NOT NULL,
   `order_status` tinyint(1) NOT NULL,
   `product_shipping_status` tinyint(1) NOT NULL,
+  `is_view` tinyint(1) NOT NULL,
   `created_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `customer_order`
+--
+
+INSERT INTO `customer_order` (`id`, `customer_id`, `product_link`, `remark`, `quantity`, `price`, `us_tax`, `mm_tax`, `commission`, `weight_cost`, `order_status`, `product_shipping_status`, `is_view`, `created_date`) VALUES
+(1, 2, 'https://www.bestchannel.thebestshopmm.com/movies/detail/91', 'some', 1, 1000, 10, 5, 7, 1, 0, 0, 0, '2020-05-28 11:21:34'),
+(2, 2, 'https://item.taobao.com/item.htm?spm=a21wu.241046-global.4691948847.3.41cab6cbq8fXLM&scm=1007.15423.84311.100200300000005&id=41068624542&pvid=6e177605-0555-4efd-b8d9-6f5f00a75bf2', 'some', 1, 1000, 10, 3, 5, 6, 2, 0, 0, '2020-05-28 12:39:41'),
+(3, 2, 'https://item.taobao.com/item.htm?spm=a21wu.241046-global.4691948847.3.41cab6cbq8fXLM&scm=1007.15423.84311.100200300000005&id=41068624542&pvid=6e177605-0555-4efd-b8d9-6f5f00a75bf2', 'some', 1, 1000, 10, 3, 5, 6, 3, 0, 0, '2020-05-28 12:40:19'),
+(4, 3, 'https://item.taobao.com/item.htm?spm=a21wu.241046-global.4691948847.3.41cab6cbq8fXLM&scm=1007.15423.84311.100200300000005&id=41068624542&pvid=6e177605-0555-4efd-b8d9-6f5f00a75bf2', 'sth', 2, 1000, 7, 6, 5, 7, 1, 0, 0, '2020-05-28 12:42:01'),
+(5, 3, 'https://item.taobao.com/item.htm?spm=a21bo.7929913.198967.13.75424174Vtm3TB&id=605150240460', 'nth', 2, 2000, 5, 5, 6, 7, 1, 0, 0, '2020-05-28 12:47:15'),
+(6, 2, 'https://item.taobao.com/item.htm?spm=a21bo.7929913.198967.13.75424174Vtm3TB&id=605150240460', 'nth', 4, 30000, 2, 3, 4, 6, 3, 0, 0, '2020-05-28 12:47:15');
 
 -- --------------------------------------------------------
 
@@ -126,7 +139,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `user_status`, `phone`, `address`, `activate_status`, `point`, `membership_id`, `created_date`, `modified_date`) VALUES
-(1, 'David', '*8201E0C1BD05201452E12ECFD5B8AFE4AEFBD053', 1, '0999999', 'mayangone', 1, 0, 1, '2020-05-26 17:20:19', '2020-05-26 09:22:12');
+(1, 'David', '*8201E0C1BD05201452E12ECFD5B8AFE4AEFBD053', 1, '0999999', 'mayangone', 1, 0, 1, '2020-05-27 14:22:34', '2020-05-26 09:22:12'),
+(2, 'Alice', '*4F5CCA657BD61D1C1127E5C4EA3B0EE4A9841B85', 1, '09798467816', 'ma thi', 1, 0, 0, '2020-05-27 14:22:34', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -170,7 +184,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `customer_order`
 --
 ALTER TABLE `customer_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `customer_statement`
@@ -194,7 +208,7 @@ ALTER TABLE `membership`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
