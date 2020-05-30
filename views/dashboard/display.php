308aa4d5d75518ac1dd7f8cd86e3f183359efdc3
SweSwe-Nyein <sweswe4720@gmail.com>
2020-05-30 14:29:42 +0630

    update dashboard display

--- views/dashboard/display.php
+++ views/dashboard/display.php
@@ -3,40 +3,8 @@ displayPageHeader('Dashboard | ' . WEB_NAME);
 displayMainNavigation('dashboard');
  ?>
  <section>
-   <div class="">
-      <a href="<?php echo URL ?>/customer/display/" class="">
-      <span id="customers_count"></span>
-      <span>Customers</span>
-    </a>
-   </div>
-   <div class="">
-     <a href="<?php echo URL ?>/order/display/" class="">
-       <span id="orders_count"></span>
-       <span>New Orders</span>
-     </a>
-   </div>
-   <div class="">
-     <a href="<?php echo URL ?>/conversation/display/" class="">
-       <span id="messages_count"></span>
-       <span>New Messages</span>
-     </a>
-   </div>
-   <div class="">
-     <a href="<?php echo URL ?>/membership/display/">
-       <span>Memberships</span>
-     </a>
-   </div>
- </section>
- <section>
-   <h1>Add New Customer</h1>
-   <form class="" action="<?php echo URL ?>/customer/create/" method="post">
-     <div class="">
-       <input type="text" name="username" placeholder="Username">
-     </div>
-     <div class="">
-       <input type="password" name="password1" placeholder="Password">
-     </div>
-  </section>
+
+   <section>
      <div class="">
         <a href="<?php echo URL ?>/customer/display/" class="">
         <span id="customers_count"></span>
@@ -60,8 +28,8 @@ displayMainNavigation('dashboard');
          <span>Memberships</span>
        </a>
      </div>
-    </section>
-    <section>
+   </section>
+   <section>
      <h1>Add New Customer</h1>
      <form class="" action="<?php echo URL ?>/customer/create/" method="post">
        <div class="">
@@ -83,8 +51,8 @@ displayMainNavigation('dashboard');
            <input type="submit" value="Create">
        </div>
      </form>
-    </section>
-  </section>
+   </section>
+ </section>
 <script src="<?php echo URL ?>/scripts/dashboard.js"></script>
 
 <?php
