47165aac30c589392909bee229c3dfab3c7e3cf0
khinyu2000 <khinyu2000@gmail.com>
2020-05-31 14:44:55 -0400

    dashboard page

--- views/settings/display.php
+++ views/settings/display.php
@@ -2,23 +2,23 @@
 displayPageHeader('Settings | ' . WEB_NAME);
 displayMainNavigation('settings');
  ?>
- <section id="ky-settings-content">
+ <section id="ky-settings-section"> <!--wrap div for rounds-->
    <i class="fas fa-user-circle"></i>
    <span>Swe Swe Nyein</span>
-   <form id="ky-account-configure-form" action="<?php echo URL ?>/settings/change_account/" method="post">
+   <form class="ky-account-configure-form" action="<?php echo URL ?>/settings/change_account/" method="post">
      <div class="">
        <span id="current-text">CURRENT</span>
        <span id="new-text">NEW</span>
-       <div id="ky-current-account-container">
+       <div class="ky-current-account-container">
          <input type="text" name="current_username" placeholder="Username">
          <input type="password" name="current_password" placeholder="Password">
        </div>
-       <div id="ky-new-account-container">
+       <div class="ky-new-account-container">
          <input type="text" name="new_username" placeholder="Username">
          <input type="password" name="new_password1" placeholder="New Password">
          <input type="password" name="new_password2" placeholder="Confirm Password">
        </div>
-       <div id="ky-change-button-container">
+       <div class="ky-change-button-container">
          <button type="submit">CHANGE</button>
        </div>
      </div>
@@ -26,12 +26,12 @@ displayMainNavigation('settings');
  </section>
  <script type="text/javascript">
  $(document).ready(function() {
-   $('#ky-current-account-container input').on('focus', function(){
+   $('.ky-current-account-container input').on('focus', function(){
    $('#new-text').removeClass('ky-onfocus');
    $('#current-text').addClass('ky-onfocus');
  });
 
- $('#ky-new-account-container input').on('focus', function(){
+ $('.ky-new-account-container input').on('focus', function(){
    $('#current-text').removeClass('ky-onfocus');
    $('#new-text').addClass('ky-onfocus');
  });
