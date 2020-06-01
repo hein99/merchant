39aa7e31b537a04a5224f9d924dd8da2bca4eea0
khinyu2000 <khinyu2000@gmail.com>
2020-06-01 10:45:35 -0400

    raw membership

--- views/membership/display.php
+++ views/membership/display.php
@@ -2,7 +2,100 @@
 displayPageHeader('Membership | ' . WEB_NAME);
 displayMainNavigation('membership');
  ?>
- <h1>Membership</h1>
+ <section id="ky-memberships-section">
+   <div class="ky-silver-container">
+     <div class="ky-membership-header">
+       <div class="ky-icon-heading-container">
+         <div class="ky-silver-icon">
+           <i class="fas fa-award"></i>
+           <span>s</span>
+         </div>
+         <span>Silver</span>
+       </div>
+       <div class="">
+         <div class="ky-silver-percentage-container">
+           <input type="text" name="" value="">
+           <div class="ky-percentage-edit"></div>    <!--switch class edit and save by using js-->
+         </div>
+       </div>
+     </div>
+     <div class="ky-silver-body">
+
+     </div>
+     <div class="ky-silver-footer">
+
+     </div>
+   </div>
+   <div class="ky-gold-container">
+     <div class="ky-membership-header">
+       <div class="ky-icon-heading-container">
+         <div class="ky-silver-icon">
+           <i class="fas fa-award"></i>
+           <span>s</span>
+         </div>
+         <span>Silver</span>
+       </div>
+       <div class="">
+         <div class="ky-silver-percentage-container">
+           <input type="text" name="" value="">
+           <div class="ky-percentage-edit"></div>    <!--switch class edit and save by using js-->
+         </div>
+       </div>
+     </div>
+     <div class="ky-silver-body">
+
+     </div>
+     <div class="ky-silver-footer">
+
+     </div>
+   </div>
+   <div class="ky-platinum-container">
+     <div class="ky-membership-header">
+       <div class="ky-icon-heading-container">
+         <div class="ky-silver-icon">
+           <i class="fas fa-award"></i>
+           <span>s</span>
+         </div>
+         <span>Silver</span>
+       </div>
+       <div class="">
+         <div class="ky-silver-percentage-container">
+           <input type="text" name="" value="">
+           <div class="ky-percentage-edit"></div>    <!--switch class edit and save by using js-->
+         </div>
+       </div>
+     </div>
+     <div class="ky-silver-body">
+
+     </div>
+     <div class="ky-silver-footer">
+
+     </div>
+   </div>
+   <div class="ky-diamond-container">
+     <div class="ky-membership-header">
+       <div class="ky-icon-heading-container">
+         <div class="ky-silver-icon">
+           <i class="fas fa-award"></i>
+           <span>s</span>
+         </div>
+         <span>Silver</span>
+       </div>
+       <div class="">
+         <div class="ky-silver-percentage-container">
+           <input type="text" name="" value="">
+           <div class="ky-percentage-edit"></div>    <!--switch class edit and save by using js-->
+         </div>
+       </div>
+     </div>
+     <div class="ky-silver-body">
+
+     </div>
+     <div class="ky-silver-footer">
+
+     </div>
+   </div>
+ </section>
 <?php
 displayPageFooter();
 ?>
