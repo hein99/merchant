691cdfe501c3246812c771e49f7430acd09328b0
HeinKhant <heinkaungkhant1999@gmail.com>
2020-06-02 00:10:20 +0630

    membership finished

--- scripts/membership.js
+++ scripts/membership.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  $('.percentage-text-js').prop("disabled", true);
+  $('.definition-text-js').prop("disabled", true);
   $('.btn-edit-percentage-js').show();
   $('.btn-save-percentage-js').hide();
 
@@ -10,12 +12,14 @@ $(document).ready(function() {
     var parent = $(this).parent();
     $(this).hide();
     $('.btn-save-percentage-js', parent).show();
+    $('.percentage-text-js', parent).prop("disabled", false);
   });
 
   $(document).on('click', '.btn-save-percentage-js', function(){
     var parent = $(this).parent();
     $(this).hide();
     $('.btn-edit-percentage-js', parent).show();
+    $('.percentage-text-js', parent).prop("disabled", true);
 
     var id = $('.percentage-text-js', parent).data('id');
     var percentage = $('.percentage-text-js', parent).val();
@@ -26,26 +30,26 @@ $(document).ready(function() {
     var parent = $(this).parent();
     $(this).hide();
     $('.btn-gp-save-text-js', parent).show();
+    $('.definition-text-js', parent).prop("disabled", false);
   });
 
   $(document).on('click', '.btn-cancel-text-js', function(){
     var parent = $(this).parent().parent();
     $('.btn-gp-save-text-js', parent).hide();
     $('.btn-edit-text-js', parent).show();
+    $('.definition-text-js', parent).prop("disabled", true);
   });
 
   $(document).on('click', '.btn-save-text-js', function(){
     var parent = $(this).parent().parent();
     $('.btn-gp-save-text-js', parent).hide();
     $('.btn-edit-text-js', parent).show();
+    $('.definition-text-js', parent).prop("disabled", true);
 
     var id = $('.definition-text-js', parent).data('id');
-    var definition = $('.definition-text-js', parent).html();
+    var definition = $('.definition-text-js', parent).val();
     editDefinitionRequest(id, definition);
   });
-
-
-
 });
 
 function editPercentRequest(id, percentage)
