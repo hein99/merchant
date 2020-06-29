c42594a1ce1c076b78098156edf3cfc3be80b247
HeinKhant <heinkaungkhant1999@gmail.com>
2020-06-29 12:05:16 +0630

    Manipulate DB file

---
+++ _Just Development/Initial DB.sql
@@ -0,0 +1,299 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 29, 2020 at 12:25 PM
+-- Server version: 5.6.43
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cp384072_wholesale`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_order`
+--
+
+CREATE TABLE `customer_order` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `product_link` varchar(255) NOT NULL,
+  `remark` text NOT NULL,
+  `cupon_code` varchar(20) NOT NULL,
+  `quantity` int(5) NOT NULL,
+  `price` float NOT NULL,
+  `us_tax` float NOT NULL,
+  `shipping_cost` float NOT NULL,
+  `first_exchange_rate` float NOT NULL,
+  `commission` float NOT NULL,
+  `product_weight` float NOT NULL,
+  `weight_cost` float NOT NULL,
+  `mm_tax` float NOT NULL,
+  `second_exchange_rate` float NOT NULL,
+  `is_deliver` tinyint(1) NOT NULL,
+  `delivery_fee` float NOT NULL,
+  `order_status` tinyint(1) NOT NULL,
+  `has_viewed_admin` tinyint(1) NOT NULL,
+  `has_viewed_customer` tinyint(1) NOT NULL,
+  `created_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_statement`
+--
+
+CREATE TABLE `customer_statement` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `amount` varchar(10) NOT NULL,
+  `amount_status` tinyint(1) NOT NULL,
+  `about` text NOT NULL,
+  `created_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exchange_rate`
+--
+
+CREATE TABLE `exchange_rate` (
+  `id` int(11) NOT NULL,
+  `mmk` float NOT NULL,
+  `created_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `exchange_rate`
+--
+
+INSERT INTO `exchange_rate` (`id`, `mmk`, `created_date`) VALUES
+(1, 1500, '2020-06-29 12:09:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_record`
+--
+
+CREATE TABLE `login_record` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `active_activity` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_type` enum('no','yes') NOT NULL,
+  `to_whom_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login_record`
+--
+
+INSERT INTO `login_record` (`id`, `user_id`, `active_activity`, `is_type`, `to_whom_id`) VALUES
+(1, 1, '2020-06-29 12:25:19', 'no', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `membership`
+--
+
+CREATE TABLE `membership` (
+  `id` int(11) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `definition` text NOT NULL,
+  `percentage` float NOT NULL,
+  `modified_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `membership`
+--
+
+INSERT INTO `membership` (`id`, `name`, `definition`, `percentage`, `modified_date`) VALUES
+(1, 'Silver', 'Initial', 15, '2020-06-29 12:14:39'),
+(2, 'Gold', 'Initial', 13, '2020-06-29 12:14:39'),
+(3, 'Platinum', 'Initial', 12, '2020-06-29 12:15:14'),
+(4, 'Diamond', 'Initial', 10, '2020-06-29 12:15:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message_record`
+--
+
+CREATE TABLE `message_record` (
+  `id` int(11) NOT NULL,
+  `to_user_id` int(11) NOT NULL,
+  `from_user_id` int(11) NOT NULL,
+  `messages` text NOT NULL,
+  `is_image` enum('yes','no') NOT NULL,
+  `arrived_time` datetime NOT NULL,
+  `admin_has_viewed` tinyint(1) NOT NULL,
+  `customer_has_viewed` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_request`
+--
+
+CREATE TABLE `password_request` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  `requested_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(41) NOT NULL,
+  `user_status` tinyint(1) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `address` text NOT NULL,
+  `activate_status` tinyint(1) NOT NULL,
+  `point` int(11) NOT NULL,
+  `balance` float NOT NULL,
+  `membership_id` int(11) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `user_status`, `phone`, `address`, `activate_status`, `point`, `balance`, `membership_id`, `created_date`, `modified_date`) VALUES
+(1, 'Hein Myat Thu', '*2ED8A76EECA697EADC433B10307B5388A0FA531E', 1, '09965353564', 'Hlaing 16 Qtr, Yangon', 1, 0, 0, 1, '2020-06-28 22:24:02', '2020-06-28 22:24:02');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer_order`
+--
+ALTER TABLE `customer_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer_statement`
+--
+ALTER TABLE `customer_statement`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `exchange_rate`
+--
+ALTER TABLE `exchange_rate`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_record`
+--
+ALTER TABLE `login_record`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `membership`
+--
+ALTER TABLE `membership`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `message_record`
+--
+ALTER TABLE `message_record`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_request`
+--
+ALTER TABLE `password_request`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer_order`
+--
+ALTER TABLE `customer_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer_statement`
+--
+ALTER TABLE `customer_statement`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `exchange_rate`
+--
+ALTER TABLE `exchange_rate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `login_record`
+--
+ALTER TABLE `login_record`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `membership`
+--
+ALTER TABLE `membership`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `message_record`
+--
+ALTER TABLE `message_record`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `password_request`
+--
+ALTER TABLE `password_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
